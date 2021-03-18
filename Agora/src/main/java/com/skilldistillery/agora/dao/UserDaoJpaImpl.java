f6151a2f10bb331ded50de0c028fe5059bfabe73
ramonarmen96 <ramonarmen96@gmail.com>
2021-03-18 16:44:00 -0600

    stub user dao, validate full stack

---
+++ Agora/src/main/java/com/skilldistillery/agora/dao/UserDaoJpaImpl.java
@@ -0,0 +1,23 @@
+package com.skilldistillery.agora.dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
+import com.skilldistillery.agora.entities.User;
+
+@Service
+@Transactional
+public class UserDaoJpaImpl implements UserDAO {
+
+	@PersistenceContext
+	private EntityManager em;
+
+	@Override
+	public User findById(int userId) {
+		return em.find(User.class, userId);
+	}
+
+}
