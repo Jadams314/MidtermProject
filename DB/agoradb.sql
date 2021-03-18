1b0cdbfdcd3bcd6da29659686a036973bf824294
erikskell <erikskell@gmail.com>
2021-03-18 18:47:03 -0500

    Added Inventory, product, shopping cart, history tables to DB

--- DB/agoradb.sql
+++ DB/agoradb.sql
@@ -29,6 +29,64 @@ CREATE TABLE IF NOT EXISTS `user` (
   PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
+
+-- -----------------------------------------------------
+-- Table `inventory`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `inventory` ;
+
+CREATE TABLE IF NOT EXISTS `inventory` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `item_name` VARCHAR(100) NULL,
+  `item_description` VARCHAR(200) NULL,
+  `seller_id` INT NULL,
+  `product_id` INT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `product`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `product` ;
+
+CREATE TABLE IF NOT EXISTS `product` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(100) NULL,
+  `description` VARCHAR(2000) NULL,
+  `bullet_id` INT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `shopping_cart`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `shopping_cart` ;
+
+CREATE TABLE IF NOT EXISTS `shopping_cart` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `user_id` INT NOT NULL,
+  `inventory_id` INT NOT NULL,
+  `isPurchased` TINYINT NULL DEFAULT 0,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `history`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `history` ;
+
+CREATE TABLE IF NOT EXISTS `history` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `user_id` INT NULL,
+  `inventory_id` INT NULL,
+  `rating` SMALLINT(5) NULL,
+  `review` VARCHAR(1000) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
 SET SQL_MODE = '';
 DROP USER IF EXISTS agorauser@localhost;
 SET SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
