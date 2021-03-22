e299ce9606dd2f670dc0b26f1dad1ff7f2224656
Joshua Rose <jlr007@protonmail.com>
2021-03-22 17:27:36 -0400

    stubbed out CartDAOImpl

--- Agora/src/main/java/com/skilldistillery/agora/dao/CartDAO.java
+++ Agora/src/main/java/com/skilldistillery/agora/dao/CartDAO.java
@@ -6,6 +6,10 @@ import com.skilldistillery.agora.entities.ShoppingCart;
 
 public interface CartDAO {
 	
-	List<ShoppingCart> getShoppingCarts(int userId);
+	List<ShoppingCart> getShoppingCart(int userId);
+	
+	void addToCart(int userId, int inventoryId);
+	
+	void removeFromCart(int userId, int inventoryId);
 
 }
