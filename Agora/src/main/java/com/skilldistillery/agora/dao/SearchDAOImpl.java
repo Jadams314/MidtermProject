4621f61bbb832e23d2a35a6e7798251cb241e2f0
ramonarmen96 <ramonarmen96@gmail.com>
2021-03-23 10:00:16 -0600

    product jsp updated

--- Agora/src/main/java/com/skilldistillery/agora/dao/SearchDAOImpl.java
+++ Agora/src/main/java/com/skilldistillery/agora/dao/SearchDAOImpl.java
@@ -17,11 +17,11 @@ public class SearchDAOImpl implements SearchDao {
 
 	@PersistenceContext
 	private EntityManager em;
-	
+
 	@Override
 	public Product findById(int id) {
 		return em.find(Product.class, id);
-		}
+	}
 
 	@Override
 	public List<Product> findAll() {
@@ -31,17 +31,14 @@ public class SearchDAOImpl implements SearchDao {
 
 	@Override
 	public List<Product> findProductByKeyword(String keyword) {
-//		
-//		select e from Employee e where lower(e.firstName) like lower(concat('%', :search, '%')) " +
-//		"or lower(e.lastName) like lower(concat('%', :search, '%'))")
 
 		String jpql = "SELECT p from Product p WHERE p.name LIKE :keyword OR p.description LIKE :keyword";
-		List<Object>results = em.createQuery(jpql, Object.class).setParameter("keyword","%" + keyword+ "%").getResultList();
-			System.out.println("***********************************"+results);
-			List<Product>list = new ArrayList<>();
+		List<Object> results = em.createQuery(jpql, Object.class).setParameter("keyword", "%" + keyword + "%")
+				.getResultList();
+		List<Product> list = new ArrayList<>();
 
-			results.stream().forEach(x -> list.add((Product)x));
-			
+		results.stream().forEach(x -> list.add((Product) x));
+
 		return list;
 	}
 }
--- Agora/src/main/webapp/WEB-INF/views/product.jsp
+++ Agora/src/main/webapp/WEB-INF/views/product.jsp
@@ -9,26 +9,36 @@
 <title>Product By ID</title>
 </head>
 <body>
+<!--Products By ID-->
 	<h1>Products:</h1>
 	<div>
 		<h5>${product.name}(${product.id})</h5>
 		<p>${product.description}</p>
 	</div>
-	<h5>${displayAll}</h5>
+<!--Products By ID-->
 
-	<table class="table table-striped table-hover">
-		<thead class="table-dark">
+	
+<!--Display all Products-->		
+	<h5>${displayAll}</h5>
+<!--Display all Products-->		
+
+<!--Products By keyword-->		
+	<table class="table">
+		<thead class="table">
 			<th>ID</th>
 			<th>Name</th>
+			<th>Description</th>
 		</thead>
 		<tbody>
 			<c:forEach var="p" items="${keywordSearch}">
 				<tr>
 					<td>${p.id}</td>
-					<td><a href="findProdByKW.do?pid=${p.id}">${p.name}</a></td>
+					<td>${p.name}</td>
+					<td>${p.description }</td>
 				</tr>
 			</c:forEach>
 		</tbody>
 	</table>
+<!--Products By keyword-->		
 </body>
 </html>
