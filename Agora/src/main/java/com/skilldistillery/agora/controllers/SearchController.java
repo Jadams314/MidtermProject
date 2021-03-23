2c783f39a67b0064cb033a1294855fcca22b6ef6
ramonarmen96 <ramonarmen96@gmail.com>
2021-03-23 15:04:19 -0600

    updated searchController to track session

--- Agora/src/main/java/com/skilldistillery/agora/controllers/SearchController.java
+++ Agora/src/main/java/com/skilldistillery/agora/controllers/SearchController.java
@@ -2,6 +2,8 @@ package com.skilldistillery.agora.controllers;
 
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,32 +11,56 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.skilldistillery.agora.dao.SearchDao;
+import com.skilldistillery.agora.dao.UserDAO;
 import com.skilldistillery.agora.entities.Product;
+import com.skilldistillery.agora.entities.User;
 
 @Controller
 public class SearchController {
-	
+
 	@Autowired
 	private SearchDao search;
-	
-	@RequestMapping(path= "findAllProd.do")
-	public String Allproducts(Model model) {
+
+	@RequestMapping(path = "findAllProd.do")
+	public String Allproducts(Model model, HttpSession session) {
 		List<Product> products = search.findAll();
 		model.addAttribute("displayAll", products);
-		return "views/product";
+		User user = (User) session.getAttribute("user");
+
+		if (user != null) {
+			session.setAttribute("user", user);
+			return "views/product";
+		} else {
+			return "index";
+		}
 	}
-	@RequestMapping(path= "findProdById.do")
-	public String findProdById(int pid, Model model) {
+
+	@RequestMapping(path = "findProdById.do")
+	public String findProdById(int pid, Model model, HttpSession session) {
 		Product product = search.findById(pid);
 		model.addAttribute("product", product);
-		return "views/product";
-	
+		User user = (User) session.getAttribute("user");
+
+		if (user != null) {
+			session.setAttribute("user", user);
+			return "views/product";
+		} else {
+			return "index";
+		}
+
 	}
-	@RequestMapping(path= "findProdByKW.do")
-	public String findProdByKW(String keywordSearch, Model model) {
+
+	@RequestMapping(path = "findProdByKW.do")
+	public String findProdByKW(String keywordSearch, Model model, HttpSession session) {
 		List<Product> products = search.findProductByKeyword(keywordSearch);
 		model.addAttribute("keywordSearch", products);
-		return "views/product";
-	
+		User user = (User) session.getAttribute("user");
+
+		if (user != null) {
+			session.setAttribute("user", user);
+			return "views/product";
+		} else {
+			return "index";
+		}
 	}
 }
