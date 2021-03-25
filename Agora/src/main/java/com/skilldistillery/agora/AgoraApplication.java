db106cac710cab4cf035301b2d0d3403db9bff35
Justin <>
2021-03-25 15:21:40 -0500

    Refactor for ec2 deployment

--- Agora/src/main/java/com/skilldistillery/agora/AgoraApplication.java
+++ Agora/src/main/java/com/skilldistillery/agora/AgoraApplication.java
@@ -2,9 +2,15 @@ package com.skilldistillery.agora;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AgoraApplication {
+public class AgoraApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AgoraApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AgoraApplication.class, args);
